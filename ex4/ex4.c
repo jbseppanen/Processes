9288c2cb3639d76ae3dd741fa41fb866672a33e4
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-30 14:58:09 -0700

    Implements ex4.

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,7 +1,7 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
-// are so many variants of the same basic call?
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
+// are so many variants of the same basic call?  Different systems, different format inputs and output needed.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,7 +10,19 @@
 
 int main(void)
 {
-    // Your code here    
-
+    int child_fork = fork();
+    if (child_fork == 0)
+    {
+        printf("Child process\n");
+        char *args[] = {"/bin/ls", NULL};
+        execvp(args[0], args);
+        // execv(args[0], args);
+        // execl(args[0], "", NULL);
+        // execlp(args[0], args[0], "-l", (char *) NULL);
+    }
+    else
+    {
+        printf("Parent process\n");
+    }
     return 0;
 }
