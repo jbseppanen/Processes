7bfca15bd2b43b1a9adbba2d46286f5dfc866819
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-30 09:04:56 -0700

    Implements ex1.c

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,6 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
-// (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
-// What happens to the variable when both the child and parent change the value of x?
+// (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?  Same as parent.
+// What happens to the variable when both the child and parent change the value of x?  Each changes, but does not affect the value in the other.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +8,18 @@
 
 int main(void)
 {
-    // Your code here
-
+    int x = 100;
+    int child_fork = fork();
+    printf("child_fork: %d\n", child_fork);
+    if (child_fork == 0)
+    {
+        x++;
+        printf("x in child process: %d\n", x);
+    }
+    else
+    {
+        x++;
+        printf("x in parent process: %d\n", x);
+    }
     return 0;
 }
